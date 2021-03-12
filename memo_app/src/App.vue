ae67401c25cc76da552ac3f552e1341544fd0599
SeijiNumata <seizi.numata@gmail.com>
2021-03-12 11:27:29 +0900

    Lintを実行、削除に不具合があったため修正

--- memo_app/src/App.vue
+++ memo_app/src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="component">
     <h1 class="title">メモアプリ</h1>
-    <div class=memo-component>
-      <div class=memo-index>
-        <li v-for="(memo) in memos" :key="memo.id">
-          <a href="#" @click="editMemo(memo)">{{memo.oneLine}}</a>
+    <div class="memo-component" >
+      <div class="memo-index">
+        <li v-for="memo in memos" :key="memo.id">
+          <a href="#" @click="editMemo(memo)"> {{memo.oneLine}}</a>
         </li>
-
         <button @click="addNewMemo" class="new-memo-button btn btn-primary">+</button>
       </div>
       <div class="text-box">
@@ -20,66 +19,70 @@
 
 <script>
   export default {
-    name: 'App',
-    data() {
-      return {
-        memos: [],
-        oneLineMemos: [],
-        edit: [],
-        memoValue: "",
-        memoState: "new",
-      }
-    },
-    methods: {
-      addNewMemo() {
-        this.memoState = "new"
-        this.newMemo = ""
-      },
-      editMemo(memo) {
-        this.memoValue = memo.content
-        this.memoState = memo.id
-      },
-      updateMemos() {
-        if (this.memoValue === "") {
-          return;
-        }
-        if (this.memoState === "new") {
-          let uuid = new Date().getTime().toString(16) + Math.floor(1000 * Math.random()).toString(16)
-          this.memos.push({
-            id: uuid,
-            content: this.memoValue,
-            oneLine: this.memoValue.split(/\r\n|\r|\n/)[0]
-          })
-        } else {
-          const num = this.memos.findIndex(item => item.id === this.memoState)
-          this.memos[num].content = this.memoValue
-          this.memos[num].oneLine = this.memoValue.split(/\r\n|\r|\n/)[0]
-        }
-        this.memoValue = ""
-        this.memoState = "new"
-        this.saveMemo(); 
-      },
-      deleteMemo() {
-        const num = this.memos.findIndex(item => item.id === this.memoState)
-        this.memos.splice(this.memos[num - 1], 1);
-        this.memoValue = ""
-        this.memoState = "new"
-        this.saveMemo(); 
-      },
-      saveMemo(){
-        localStorage.setItem('memos', JSON.stringify(this.memos));
-      },
-      loadMemos: function(){
-          this.memos = JSON.parse( localStorage.getItem('memos') );
-          if( !this.memos ){
-            this.memos = [];
-          }
-        },
-    },
-    mounted: function(){
-      this.loadMemos();
+  name: 'App',
+  data () {
+    return {
+      memos: [],
+      oneLineMemos: [],
+      edit: [],
+      memoValue: '',
+      memoState: 'new'
+    }
   },
+  methods: {
+    addNewMemo () {
+      this.memoState = 'new'
+      this.newMemo = ''
+      this.memoValue=''
+    },
+    editMemo (memo) {
+      this.memoValue = memo.content
+      this.memoState = memo.id
+    },
+    updateMemos () {
+      if (this.memoValue === '') {
+        return
+      }
+      if (this.memoState === 'new') {
+        const uuid = new Date().getTime().toString(16) + Math.floor(1000 * Math.random()).toString(16)
+        this.memos.push({
+          id: uuid,
+          content: this.memoValue,
+          oneLine: this.memoValue.split(/\r\n|\r|\n/)[0]
+        })
+      } else {
+        const num = this.memos.findIndex(item => item.id === this.memoState)
+        this.memos[num].content = this.memoValue
+        this.memos[num].oneLine = this.memoValue.split(/\r\n|\r|\n/)[0]
+      }
+      this.memoValue = ''
+      this.memoState = 'new'
+      this.saveMemo()
+    },
+    deleteMemo () {
+      
+      const num = this.memos.findIndex(item => item.id === this.memoState)
+      console.log(num)
+      this.memos.splice(num, 1)
+      console.log(this.memos)
+      this.memoValue = ''
+      this.memoState = 'new'
+      this.saveMemo()
+    },
+    saveMemo () {
+      localStorage.setItem('memos', JSON.stringify(this.memos))
+    },
+    loadMemos: function () {
+      this.memos = JSON.parse(localStorage.getItem('memos'))
+      if (!this.memos) {
+        this.memos = []
+      }
+    }
+  },
+  mounted: function () {
+    this.loadMemos()
   }
+}
 </script>
 
 <style>
